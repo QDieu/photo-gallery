f12162b08bbfef613476f153e8860a3b08f832c6
qdieu <navvy3@gmail.com>
2022-02-14 11:20:40 +0200

    feat: add base ui view

---
+++ src/shared/ui/core/molecules/card-row/card-row.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TPhoto } from '../../../../types/Photo';
+import { CardItem } from '../../atoms';
+
+const GridWrapper = styled.div``;
+
+const Wrapper = styled.div`
+    background-color: skyblue;
+
+    display: grid;
+
+    gap: 15px;
+    grid-template-columns: repeat(4, 1fr);
+
+    & ${GridWrapper}:nth-child(4n - 3) {
+        grid-column: 1/4;
+    }
+
+    & ${GridWrapper}:nth-child(4n - 2) {
+        grid-column: 4/5;
+    }
+
+    & ${GridWrapper}:nth-child(4n - 1) {
+        grid-column: 1/2;
+    }
+
+    & ${GridWrapper}:nth-child(4n) {
+        grid-column: 2/5;
+    }
+`;
+
+type TProps = {
+    photos: Array<TPhoto>;
+};
+
+export const CardRow: React.FC<TProps> = ({ photos }) => {
+    return (
+        <Wrapper>
+            {photos.map((item) => (
+                <GridWrapper>
+                    <CardItem
+                        albumId={item.albumId}
+                        id={item.id}
+                        title={item.title}
+                        url={item.thumbnailUrl}
+                    />
+                </GridWrapper>
+            ))}
+        </Wrapper>
+    );
+};
